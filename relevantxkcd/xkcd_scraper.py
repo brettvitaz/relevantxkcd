5dfd789733c31765e9d3972c31cac7351bcdabae
Brett Vitaz <brett@vitaz.net>
2015-08-04 11:17:00 -0700

    [scraper] Improve code for downloading comic data. - Query database for last comic number and start from there. - Store downloaded comics in database.

--- relevantxkcd/xkcd_scraper.py
+++ relevantxkcd/xkcd_scraper.py
@@ -1,26 +1,67 @@
-import json
 import requests
 import time
+import sys
+
+from sqlalchemy.sql.expression import func
+
+from relevantxkcd.database import Session, Comic
 
 URL = 'http://xkcd.com/{:d}/info.0.json'
 
 
 def get_all_the_things():
+    session = Session()
 
-    idx = 0
-    while idx < 1:
+    try:
+        query = session.query(func.max(Comic.num)).scalar()
+        idx = query or 0
+        print('Starting with comic number:', idx)
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+    complete = False
+
+    while not complete:
         idx += 1
+
+        # Skip some comics that intentionally cause a bad response. (Only 404 as of now.)
         if idx in [404]:
             idx += 1
-        f = requests.get(URL.format(idx))
-        if f.status_code == 404:
-            print('finished')
-            break
-        while f.status_code == 503:
+
+        # Sometimes getting the response times out. Keep trying.
+        response = None
+        tries = 0
+        while response is None or response.status_code != 200:
+            if tries >= 5:
+                print('Download failed. Aborting')
+                sys.exit(1)
+
+            tries += 1
             time.sleep(1.0)
-            f = requests.get(URL.format(idx), timeout=1.0)
-        print(f.json())
+            response = requests.get(URL.format(idx), timeout=5.0)
 
+            if response.status_code != 200:
+                print('Failed to download comic number: {:d}. Tried {:d} times. Response: {}'
+                      .format(idx, tries, response.status_code))
 
-if __name__ == '__main__':
-    get_all_the_things()
+            # A response code of 404 should indicate that there are no more comics available to download.
+            if response.status_code == 404:
+                print('Finished')
+                complete = True
+                break
+
+        if not complete:
+            # Gather only the items with keys we recognize.
+            kwargs = {k: v for k, v in response.json().items() if k in Comic.__table__.columns}
+            comic = Comic(**kwargs)
+            try:
+                session.add(comic)
+                session.commit()
+            except:
+                session.rollback()
+                raise
+            finally:
+                session.close()
