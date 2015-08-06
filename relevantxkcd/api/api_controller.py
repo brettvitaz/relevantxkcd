dd9329e6451f987bcc4477bb3bf32d17f5fed294
Brett Vitaz <brett@vitaz.net>
2015-08-05 22:34:51 -0700

    [api_controller] Search database for query string. Build a list of possible results.

--- relevantxkcd/api/api_controller.py
+++ relevantxkcd/api/api_controller.py
@@ -1,32 +1,55 @@
 import bottle
+import re
+
+from relevantxkcd.database import session_scope, Comic
+
+URL = 'http://xkcd.com/'
+
+PATTERNS = [
+    r'\b%s\b',
+    r'\b%s',
+    r'%s',
+]
 
 
-@bottle.get('/xkcd')
-def get_xkcd(self):
-    return '''
-        <form action="/api/xkcd" method="post">
-            Query: <input name="query" type="text" />
-            <input value="Submit" type="submit" />
-        </form>
-    '''
+class ApiController:
+    @staticmethod
+    @bottle.get('/xkcd')
+    def get_query():
+        return '''
+<form action="/api/xkcd" method="post">
+    Query: <input name="query" type="text" />
+    <input value="Submit" type="submit" />
+</form>
+        '''
 
+    @staticmethod
+    @bottle.get('/api/xkcd/<query>')
+    def query_subject(query):
+        result_urls = None
+        with session_scope() as session:
+            results = session.query(Comic.num, Comic.safe_title).filter(Comic.safe_title.like('%{:s}%'.format(query))).all()
+            print('Results:', len(results))
+            new_results = []
+            while len(new_results) == 0:
+                for pattern in PATTERNS:
+                    for result in results:
+                        if re.findall(pattern % (query,), result.safe_title, re.IGNORECASE):
+                            if result not in new_results:
+                                new_results.append(result)
+            for result in new_results:
+                result_urls = (result_urls or '') + r'<p><a href="{url}{num}">{title}</a></p>'.format(title=result.safe_title, url=URL, num=result.num)
+        return bottle.template('</p>Query was {{query}}</p><p>Search results: </p>{{!url}}', query=query, url=result_urls)
 
-@bottle.get('/api/xkcd/<query>')
-def route_xkcd(self, query):
-    return bottle.template('</p>Query was {{query}}</p>', query=query)
-
-
-@bottle.get('/api/xkcd')
-@bottle.post('/api/xkcd')
-def do_xkcd(self):
-    try:
-        if bottle.request.json:
-            query = bottle.request.json['query']
-        else:
-            query = bottle.request.forms['query']
-        return bottle.template('</p>Query was {{query}}</p>', query=query)
-    except KeyError:
-        bottle.abort(500, 'Request did not contain a query.')
-
-
-bottle.run(host='localhost', port='8080', debug=True)
+    @staticmethod
+    @bottle.get('/api/xkcd')
+    @bottle.post('/api/xkcd')
+    def handle_query():
+        try:
+            if bottle.request.json:
+                query = bottle.request.json['query']
+            else:
+                query = bottle.request.forms['query']
+            return ApiController.query_subject(query)
+        except KeyError:
+            bottle.abort(500, 'Request did not contain a query.')
